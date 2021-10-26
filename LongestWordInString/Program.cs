84bac5c3afcb22dde43882fbe5e5f43f451a8ba0
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-10-26 19:45:25 -0400

    working

--- LongestWordInString/Program.cs
+++ LongestWordInString/Program.cs
@@ -6,7 +6,25 @@ namespace LongestWordInString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "This is a test sentence for my new program.";
+            Console.WriteLine(input);
+            Console.WriteLine(FindLongestWord(input));
+            Console.ReadKey();
+        }
+
+
+        static string FindLongestWord(string input)
+        {
+            string[] words = input.Split(" ", StringSplitOptions.None);
+            string longestWord = "";
+            foreach(string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            return longestWord;
         }
     }
 }
